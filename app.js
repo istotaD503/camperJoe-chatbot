34110f4c1915e3a0d104d29a5d8f85a27807b707
Alex Mazhavy <alexander.mazhavy@gmail.com>
2018-06-18 01:29:41 -0400

    Final Submit

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
 const chalk = require('chalk');
-const { newsDefault } = require('./utils')
+const { newsDefault } = require('./utils');
+const here = chalk.red('HERE ==>');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -30,10 +31,6 @@ const connector = new builder.ChatConnector({
 
 app.post('/api/messages', connector.listen());
 
-// default conversation should state what options are avaivable for the user
-
-// #1 user talks to Joe the furst time:
-// Tell user what he can do
 var inMemoryStorage = new builder.MemoryBotStorage();
 const optionsJoe = 'Planning a trip | News about camping';
 
@@ -44,72 +41,139 @@ const bot = new builder.UniversalBot(connector, [
 bot.dialog('greeting', [
   session => {
     session.send(`Hi, I'm Joe and I'm camping expert! <br> `);
-    session.send(`I can help you to plan your trip or tell you other interesting info about camping.`)
-    session.endDialog(`Please say: 'plan a trip' or 'read news'.`)
+    session.send(
+      `I can help you to plan your trip or tell you other interesting info about camping.`
+    );
+    session.endDialog(`Please say: 'plan a trip' or 'read news'.`);
   }
 ]);
 
-bot.dialog('options', (session, args, next) => {
-    builder.Prompts.choice(session, `I can help you to:`, `plan a trip|read news`, {listStyle : 3})
-    session.endDialog()
-})
-.triggerAction({
-    matches: /^help$/i,
-});
+bot
+  .dialog('options', (session, args, next) => {
+    builder.Prompts.choice(
+      session,
+      `I can help you to:`,
+      `plan a trip|read news`,
+      { listStyle: 3 }
+    );
+    session.endDialog();
+  })
+  .triggerAction({
+    matches: /^help$/i
+  });
 
-bot.dialog('readNews', [
+bot
+  .dialog('readNews', [
     session => {
-        session.send(`Sounds good, let me find smth cool for you!`)
-        builder.Prompts.text(session, `You can tell me the topic or just say 'latest'`)
+      session.send(`Sounds good, let me find smth cool for you!`);
+      builder.Prompts.text(
+        session,
+        `You can tell me the topic or just say 'latest'`
+      );
     },
     (session, results) => {
-        let searchTerm = results.response == `latest` ? 'Outdoors adventure news' : results.response
-        newsDefault(session, searchTerm)
+      let searchTerm =
+        results.response == `latest`
+          ? 'Outdoors adventure news'
+          : results.response;
+      newsDefault(session, searchTerm);
     }
-])
-.triggerAction({
+  ])
+  .triggerAction({
     matches: /^read news$/i,
-    confirmPrompt: "This will cancel your current request. Are you sure?"
-});
+    confirmPrompt: 'This will cancel your current request. Are you sure?'
+  });
 
-
-bot.dialog('tripPlan', [
-    session => { 
-        session.send(`OK, let's plan!`)
-        session.beginDialog('askForDate')
+bot
+  .dialog('tripPlan', [
+    session => {
+      session.send(`OK, let's plan!`);
+      session.beginDialog('askForDate');
     },
     (session, results) => {
-        session.dialogData.date = builder.EntityRecognizer.resolveTime([results.response])
-        session.beginDialog('askForPlace')
+      session.dialogData.date = results.response.date;
+      session.beginDialog('askForDuration');
     },
     (session, results) => {
-        session.dialogData.place = results.response
-        session.endDialog('OK, your trip is saved!')
+      session.dialogData.numOfNights = results.response;
+      session.beginDialog('askForPlace');
+    },
+    (session, results) => {
+      let dialog = results.response.index ? 'searchForCamp' : 'searchClosest';
+      session.beginDialog(dialog);
+    },
+    (session, results) => {
+      session.dialogData.place = results.response;
+      session.endDialog('OK, your trip is saved!');
     }
-])
-.triggerAction({
+  ])
+  .triggerAction({
     matches: /^plan a trip$/i,
-    confirmPrompt: "This will cancel your current request. Are you sure?"
-});
+    confirmPrompt: 'This will cancel your current request. Are you sure?'
+  });
 
 // local help to find a place!
 
+bot.dialog('askForDuration', [
+  session =>
+    builder.Prompts.number(
+      session,
+      'How many nights are you planning to stay?'
+    ),
+  (session, results) => {
+    let numOfNights = results.response;
+    session.send(`Perfect! You'll be camping for ${numOfNights} nights.`);
+    session.endDialogWithResult(results);
+  }
+]);
+
 bot.dialog('askForDate', [
-    session => builder.Prompts.time(session, 'Please provide a date of the trip'),
-    (session, results) => {
-        session.send(`You a planning to camp at ${builder.EntityRecognizer.resolveTime([results.response])}`)
-        session.endDialogWithResult(results)
-    }
-])
+  session =>
+    builder.Prompts.time(session, 'When are you planning to go camping?'),
+  (session, results) => {
+    let date = builder.EntityRecognizer.resolveTime([results.response]);
+    session.send(`${date}, sounds good!`);
+    results.response.date = date;
+    session.endDialogWithResult(results);
+  }
+]);
 
-bot.dialog('askForPlace', [
-    session => builder.Prompts.text(session, `Do you know where you want to go?`),
+bot.dialog('locationHelp', [
+  session =>
+    builder.Prompts.choice(
+      session,
+      'Sure! Do you want me to pick something close by?',
+      'Help me pick | I have and idea!',
+      { listStyle: 3 }
+    ),
+  (session, results) => {
+    let msg = results.response.index
+      ? `Wonderful! Let me know and I'll help you to book a campground nearby <br> or search for activities`
+      : `OK, no problem! Give me some time, I'll search for something good...`;
+    session.send(msg);
+    session.endDialogWithResult(results);
+  }
+]);
+
+bot.dialog('searchForCamp', [
+  session => {
+    session.send('Searching..');
+  }
+]);
+
+bot.dialog('searchClosest', [
+  session => {
+    session.send('Searching for closest...');
+  }
+]);
+
+bot
+  .dialog('askForPlace', [
+    session => {
+        builder.Prompts.text(session, `Do you know where you want to go?`);
+    },
     (session, results) => session.endDialogWithResult(results)
-])
-
-
-
-
-
-
-
+  ])
+  .beginDialogAction('helpWithLocation', 'locationHelp', {
+    matches: /^help me$/i
+  });
